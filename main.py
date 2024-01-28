e295f6f971ab2cacc72660669e5ff9640b4b7828
Patrick D <patrick.montejo@gmail.com>
2024-01-27 23:13:06 -0500

    added file in github

---
+++ main.py
@@ -0,0 +1,8 @@
+# Determine if it's odd or even number
+
+number = int(input("Please provide number: "))
+
+if number % 2 == 0:
+    print(f" {number} is an Even number")
+else:
+    print(f" {number} is an Odd number")
